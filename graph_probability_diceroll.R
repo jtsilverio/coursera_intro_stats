9b431ea6582b21b2d6529d2bb0f0fa613ebb3e5f
Jefferson <jfo.txt@gmail.com>
2021-05-24 10:46:31 -0300

    week_2

--- graph_probability_diceroll.R
+++
@@ -1,9 +0,0 @@
-data = as.data.frame(NULL)
-for (roll in 1:1000) {
-  s = sample(x = 1:6, size = roll, replace = T)
-  p = length(s[s == 1])/roll
-  data = rbind(data, cbind(roll, p))
-  
-}
-head(data)
-plot(x = data$roll, data$p, type = "l", log="x")
